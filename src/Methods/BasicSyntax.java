8ba241503f3927721b8ba8073d52bfd564db92ff
sanacreatespace <sana.createspace@gmail.com>
2026-04-08 00:53:26 +0530

    Added new Java file

---
+++ src/Methods/BasicSyntax.java
@@ -0,0 +1,11 @@
+package Methods;
+public class BasicSyntax {
+    public static void method() {
+        System.out.println("Sana");
+    }
+
+    public static void main() {
+        System.out.println("SANA");
+        method();
+    }
+}
