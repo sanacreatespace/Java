f4fa2f0cb22132f9c99d515383c9c268fdb48f82
sanacreatespace <sana.createspace@gmail.com>
2026-04-10 10:45:53 +0530

    Added concept of return type

---
+++ src/Methods/returntype.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class returntype {
+    public static int method(){
+        System.out.println("Sana");
+        return 5;
+    }
+    public static void main(){
+        System.out.print(method()); // acts as a value
+    }
+}
