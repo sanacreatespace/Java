b1dd882bd18001130065c007d80b496ba7edd794
sanacreatespace <sana.createspace@gmail.com>
2026-04-12 22:05:48 +0530

    Added new method file

---
+++ src/Methods/maxoftwo.java
@@ -0,0 +1,10 @@
+package Methods;
+public class maxoftwo {
+    public static int max(int a, int b){
+        if(a>b) return a;
+        else return b;
+    }
+    public static void main(String[]args){
+        System.out.println(max(4, 7));
+    }
+}
